1c9e64c73bebee20e259fdea220755dbc52ab8d1
Jason Roman <funjasonroman@gmail.com>
2022-08-10 11:24:23 -0600

    Added player

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class House:
 	def draw(self) -> pygame.Surface:
 		house = pygame.Surface(self.house.get_size(), pygame.SRCALPHA)
 		house.blit(self.house, (0, 0))
-		house.set_alpha(50)
+		house.set_alpha(130)
 		combined = pygame.Surface((self.treeoffset + (len(self.trees) * 80) + 80, 300), pygame.SRCALPHA)
 		combined.blit(house, (0, 0))
 		cum_x = self.treeoffset + 0
@@ -53,6 +53,8 @@ class House:
 		return combined
 
 world: "list[House]" = []
+playerpos: "list[int, int]" = [0, 150] # CENTER position of player
+playerv: "list[int, int]" = [0, 0] # Velocity of player
 
 c = pygame.time.Clock()
 running = True
@@ -64,8 +66,9 @@ while running:
 			screensize = event.size
 			screen = pygame.display.set_mode(screensize, pygame.RESIZABLE)
 	# Drawing
-	screen.fill((0, 0, 0))
-	cum_x = 0
+	screen.fill((150, 255, 255))
+	scroll = (screensize[0] / 2) - playerpos[0]
+	cum_x = scroll + 0
 	for h in world:
 		s = h.draw()
 		screen.blit(s, (cum_x, screensize[1] - s.get_height()))
@@ -73,6 +76,18 @@ while running:
 	# Adding new houses
 	if cum_x < screensize[0]:
 		world.append(House())
+	# Draw the player
+	playersize = 10
+	pygame.draw.rect(screen, (0, 0, 0), pygame.Rect((screensize[0] / 2) - (playersize / 2), (screensize[1] - playerpos[1]) - (playersize / 2), playersize, playersize))
+	# Tick the player
+	playerpos[0] += playerv[0]
+	playerpos[1] += playerv[1]
+	if playerpos[1] < playersize / 2:
+		playerpos[1] = playersize / 2
+		playerv[1] = 0
+	else:
+		playerv[1] -= 0.1
+	playerv[0] *= 0.995
 	# Flip
 	pygame.display.flip()
 	c.tick(60)
