f16dbe846aa6e9867e877bfe8d6203565c2231e2
Jason Roman <funjasonroman@gmail.com>
2022-08-10 11:09:05 -0600

    Added tree, house graphics

--- main.py
+++ main.py
@@ -1,7 +1,58 @@
 import pygame
+import random
 
-screensize = (500, 500)
-screen = pygame.display.set_mode(screensize, pygame.RESIZABLE)
+screensize: "tuple[int, int]" = (500, 500)
+screen: pygame.Surface = pygame.display.set_mode(screensize, pygame.RESIZABLE)
+
+def drawHouse() -> pygame.Surface:
+	house: pygame.Surface = pygame.Surface((300, 300), pygame.SRCALPHA)
+	house.fill((255, 255, 255, 0))
+	# Base
+	pygame.draw.rect(house, (200, 200, 255), pygame.Rect(50, 100, 200, 200))
+	pygame.draw.rect(house, (50, 0, 10), pygame.Rect(50, 100, 200, 200), 10)
+	pygame.draw.polygon(house, (200, 200, 255), ((50, 100), (150, 0), (250, 100)))
+	pygame.draw.polygon(house, (50, 0, 10), ((50, 100), (150, 0), (250, 100)), 10)
+	# Door
+	pygame.draw.rect(house, (100, 100, 100), pygame.Rect(85, 200, 50, 90))
+	pygame.draw.circle(house, (0, 0, 0), (100, 245), 5)
+	# Window
+	pygame.draw.rect(house, (150, 150, 100), pygame.Rect(160, 135, 60, 60))
+	pygame.draw.rect(house, (50, 0, 10), pygame.Rect(160, 135, 60, 60), 10)
+	pygame.draw.line(house, (50, 0, 10), (190, 135), (190, 195), 10)
+	pygame.draw.line(house, (50, 0, 10), (160, 165), (220, 165), 10)
+	return house
+
+def drawTree() -> pygame.Surface:
+	tree: pygame.Surface = pygame.Surface((200, 200), pygame.SRCALPHA)
+	tree.fill((255, 255, 255, 0))
+	treeX = 50
+	treeWidth = random.randint(30, 65)
+	treeHeight = random.randint(50, 100) + (treeWidth / 2)
+	# Base
+	pygame.draw.rect(tree, (100, 50, 0), pygame.Rect(treeX, 200 - treeHeight, treeWidth, treeHeight))
+	pygame.draw.rect(tree, (50, 0, 10), pygame.Rect(treeX, 200 - treeHeight, treeWidth, treeHeight), 10)
+	# Leaves
+	pygame.draw.circle(tree, (0, 150, 0), (treeX + (treeWidth / 2), 200 - treeHeight), 50)
+	return {"img": tree, "hitbox": pygame.Rect(treeX, 200 - treeHeight, treeWidth, treeHeight)}
+
+class House:
+	def __init__(self):
+		self.house = drawHouse()
+		self.trees = [drawTree(), drawTree(), drawTree()]
+		self.treeoffset = random.randint(140, 200)
+	def draw(self) -> pygame.Surface:
+		house = pygame.Surface(self.house.get_size(), pygame.SRCALPHA)
+		house.blit(self.house, (0, 0))
+		house.set_alpha(50)
+		combined = pygame.Surface((self.treeoffset + (len(self.trees) * 80) + 80, 300), pygame.SRCALPHA)
+		combined.blit(house, (0, 0))
+		cum_x = self.treeoffset + 0
+		for tree in self.trees:
+			combined.blit(tree["img"], (cum_x, 100))
+			cum_x += 80
+		return combined
+
+display = House().draw()
 
 c = pygame.time.Clock()
 running = True
@@ -12,6 +63,7 @@ while running:
 		if event.type == pygame.VIDEORESIZE:
 			screensize = event.size
 			screen = pygame.display.set_mode(screensize, pygame.RESIZABLE)
-	screen.fill((255, 255, 255))
+	screen.fill((0, 0, 0))
+	screen.blit(display, (100, 100))
 	pygame.display.flip()
 	c.tick(60)
